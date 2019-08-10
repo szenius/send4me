b4fdf753266cc3ce07a1251affd56eb47433aba4
Sze Ying <ting.szeying@gmail.com>
2019-08-10 18:22:09 +0800

    Refactoring

--- Sender.js
+++ Sender.js
@@ -1,35 +1,12 @@
-const Telegraf = require("telegraf");
+const Telegraf = require('telegraf');
+const {savedDate, inSchedule} = require('./DateUtils.js');
 
 const bot = new Telegraf(process.env.BOT_TOKEN, { polling: true });
 const sentDates = [];
 
-const isSameDate = (date1, date2) => {
-  return (
-    date1.getDate() === date2.getDate() &&
-    date1.getMonth() === date2.getMonth() &&
-    date1.getYear() === date2.getYear()
-  );
-};
-
-const savedDate = newDate => {
-  console.log(`new date: ${newDate}`);
-  console.log(`savedDates: ${sentDates}`);
-  for (const sentDate of sentDates) {
-    if (isSameDate(sentDate, newDate)) {
-      console.log(`same date! ${sentDate} & ${newDate}`);
-      return true;
-    }
-  }
-  return false;
-};
-
-const inSchedule = date => {
-  return date.getDate() % 2 == 0;
-};
-
-while (sentDates.length === 0) {
+while (sentDates.length === 0) { // TODO: use infinite loop with timeout
   let today = new Date();
-  if (inSchedule(today) && !savedDate(today)) {
+  if (inSchedule(today) && !savedDate(today, sentDates)) {
     console.log("Sending message...");
     bot.telegram.sendMessage('-1001205975376', "Hello").catch(console.error);
     sentDates.push(today);
