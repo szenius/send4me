4f616cef9830701c4ed4a50817964f0224c0c683
Sze Ying <ting.szeying@gmail.com>
2019-08-11 10:34:34 +0800

    Inline keyboard actions now edit original message text

--- src/Bot.js
+++ src/Bot.js
@@ -3,24 +3,26 @@ const Telegraf = require("telegraf");
 const { Markup } = Telegraf;
 const Promise = require("bluebird");
 const { savedDate, inSchedule } = require("./utils/DateUtils.js");
-const { markComing, markNotComing } = require("./RequestHandler.js");
-const {
-  ACTION_COMING,
-  ACTION_NOTCOMING
-} = require("./constants/Actions.js");
-const { buildRsvp } = require("./rsvp/DefaultRsvpBuilder.js");
+const { ACTION_COMING, ACTION_NOTCOMING } = require("./constants/Actions.js");
+const { buildRsvp, updateRsvpString } = require("./rsvp/DefaultRsvpBuilder.js");
 
 const bot = new Telegraf(process.env.BOT_TOKEN, { polling: true });
 const sentDates = []; // TODO: save to database
+const coming = [];
+const notComing = [];
+const eventName = "reading session";
+const dateString = "Friday, 23 Aug 7.50PM";
 
 bot.action(ACTION_COMING, ctx => {
-  markComing(ctx.from.id);
-  ctx.reply("wuhu");
+  console.log(`${ctx.from.id} is coming`);
+  coming.push(ctx.from);
+  ctx.editMessageText(updateRsvpString(eventName, dateString, coming, notComing), defaultRsvpMenu);
 });
 
 bot.action(ACTION_NOTCOMING, ctx => {
-  markNotComing(ctx.from.id); // TODO:
-  ctx.reply("no why");
+  console.log(`${ctx.from.id} is not coming`);
+  notComing.push(ctx.from);
+  ctx.editMessageText(updateRsvpString(eventName, dateString, coming, notComing), defaultRsvpMenu);
 });
 
 const defaultRsvpMenu = Markup.inlineKeyboard([
@@ -33,7 +35,7 @@ const run = () => {
   console.log("Checking if should send message...");
   if (inSchedule(today) && !savedDate(today, sentDates)) {
     console.log("Sending message...");
-    const message = buildRsvp("reading session", "Friday, 23 Aug 7.50PM"); // TODO: read from schedule
+    const message = buildRsvp(eventName, dateString); // TODO: read from schedule
     bot.telegram.sendMessage(process.env.CHAT_ID, message, defaultRsvpMenu);
     console.log("Sent message");
     sentDates.push(today);
--- src/rsvp/DefaultRsvpBuilder.js
+++ src/rsvp/DefaultRsvpBuilder.js
@@ -2,6 +2,21 @@ const buildRsvp = (eventName, dateString) => {
   return `Hi volunteers, the next ${eventName} will be on ${dateString}. Please indicate your attendance below!\n\ncoming:\n\nnot coming(reason):`;
 };
 
+const updateRsvpString = (eventName, dateString, coming, notComing) => {
+  return `Hi volunteers, the next ${eventName} will be on ${dateString}. Please indicate your attendance below!\n\ncoming:\n${generateUserString(
+    coming
+  )}\nnot coming(reason):\n${generateUserString(notComing)}`;
+};
+
+const generateUserString = users => {
+  userString = "";
+  for (const user of users) {
+    userString += user.first_name + "\n";
+  }
+  return userString;
+};
+
 module.exports = {
-  buildRsvp
+  buildRsvp,
+  updateRsvpString
 };
