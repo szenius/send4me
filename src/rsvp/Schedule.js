bbf32460965c83394c3b960c0a4ab5e63b45a55d
Sze Ying <ting.szeying@gmail.com>
2019-08-13 22:57:58 +0800

    Populate with actual schedule

--- src/rsvp/Schedule.js
+++ src/rsvp/Schedule.js
@@ -11,16 +11,34 @@ const getEvent = date => {
 
 const events = [
   {
-    date: new Date("August 13 2019 22:42"),
+    date: new Date("August 20 2019 10:00"),
     eventName: "reading session",
-    dateString: "this Friday 16 August 8PM",
-    deadline: new Date("August 13 2019 22:43")
+    dateString: "this Friday 23 August 7.50PM",
+    deadline: new Date("August 23 2019 20:00")
   },
   {
-    date: new Date("August 13 2019 22:44"),
-    eventName: "art session",
-    dateString: "this Friday 16 August 9PM",
-    deadline: new Date("August 13 2019 22:45")
+    date: new Date("August 27 2019 10:00"),
+    eventName: "reading session",
+    dateString: "this Friday 30 August 7.50PM",
+    deadline: new Date("August 30 2019 20:00")
+  },
+  {
+    date: new Date("September 3 2019 10:00"),
+    eventName: "reading session",
+    dateString: "this Friday 6 Sept 7.50PM",
+    deadline: new Date("September 6 2019 20:00")
+  },
+  {
+    date: new Date("September 17 2019 10:00"),
+    eventName: "recap session",
+    dateString: "this Friday 20 Sept 7.50PM",
+    deadline: new Date("September 20 2019 20:00")
+  },
+  {
+    date: new Date("October 8 2019 10:00"),
+    eventName: "recap session",
+    dateString: "this Friday 11 Oct 7.50PM",
+    deadline: new Date("October 11 2019 20:00")
   }
 ];
 
