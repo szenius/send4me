59571a391154487e6664b8ad4a5a1133d7b68553
Sze Ying <ting.szeying@gmail.com>
2019-08-21 21:17:11 +0800

    Add comments, add spaces, etc

--- src/Bot.js
+++ src/Bot.js
@@ -27,18 +27,29 @@ const bot = new Telegraf(process.env.BOT_TOKEN, { polling: true });
 let activeRsvp = null;
 const sentDates = [];
 
+/**
+ * Inline menu for Default RSVPs
+ */
 const defaultRsvpMenu = Markup.inlineKeyboard([
   Markup.callbackButton(MENU_BUTTON_TEXT_COMING, ACTION_COMING),
   Markup.callbackButton(MENU_BUTTON_TEXT_NOT_COMING, ACTION_NOT_COMING)
 ]).extra();
 
+/**
+ * Steps to be executed when user clicks on "coming" button on RSVP.
+ */
 bot.action(ACTION_COMING, ctx => {
+  // If there is no active RSVP, do nothing.
   if (!activeRsvp) {
     console.log("No active RSVP is present. Perhaps the bot crashed?");
     return;
   }
+
+  // Check if coming and notcoming arrays are present in activeRSVP. If not, set to empty arrays.
   activeRsvp.coming = activeRsvp.coming ? activeRsvp.coming : [];
   activeRsvp.notComing = activeRsvp.notComing ? activeRsvp.notComing : [];
+
+  // If user tries to RSVP for old event (not the current active one), do nothing.
   if (ctx.update.callback_query.message.message_id !== activeRsvp.messageId) {
     console.log(
       `${ctx.from.first_name} (${
@@ -48,6 +59,8 @@ bot.action(ACTION_COMING, ctx => {
     return;
   }
   console.log(`${ctx.from.first_name} (${ctx.from.username}) is coming`);
+
+  // If user has already said they will come, do nothing.
   if (foundObjectInArray(ctx.from, activeRsvp.coming)) {
     console.log(
       `${ctx.from.first_name} (${
@@ -56,18 +69,25 @@ bot.action(ACTION_COMING, ctx => {
     );
     return;
   }
+
+  // Mark user as coming.
   removeObjectFromArray(ctx.from, activeRsvp.notComing);
   activeRsvp.coming.push(ctx.from);
   updateRsvpMessage(ctx);
 });
 
 bot.action(ACTION_NOT_COMING, ctx => {
+  // If there is no active RSVP, do nothing.
   if (!activeRsvp) {
     console.log("No active RSVP is present. Perhaps the bot crashed?");
     return;
   }
+
+  // Check if coming and notcoming arrays are present in activeRSVP. If not, set to empty arrays.
   activeRsvp.coming = activeRsvp.coming ? activeRsvp.coming : [];
   activeRsvp.notComing = activeRsvp.notComing ? activeRsvp.notComing : [];
+
+  // If user tries to RSVP for old event (not the current active one), do nothing.
   if (ctx.update.callback_query.message.message_id !== activeRsvp.messageId) {
     console.log(
       `${ctx.from.first_name} (${
@@ -77,6 +97,8 @@ bot.action(ACTION_NOT_COMING, ctx => {
     return;
   }
   console.log(`${ctx.from.first_name} (${ctx.from.username}) is not coming`);
+
+  // If user has already said they will not come, do nothing.
   if (foundObjectInArray(ctx.from, activeRsvp.notComing)) {
     console.log(
       `${ctx.from.first_name} (${
@@ -85,29 +107,37 @@ bot.action(ACTION_NOT_COMING, ctx => {
     );
     return;
   }
+
+  // Mark user as not coming.
   removeObjectFromArray(ctx.from, activeRsvp.coming);
   activeRsvp.notComing.push(ctx.from);
   updateRsvpMessage(ctx);
 });
 
+/**
+ * Disable old RSVPs by removing the inline menu for these old messages.
+ * This is necessary for our current assumption that there can only be one active RSVP at any point in time.
+ */
 const disableOldRsvp = () => {
   bot.telegram.editMessageText(
     process.env.CHAT_ID,
     activeRsvp.messageId,
     activeRsvp.messageId,
-    addDisabledRsvpHeader(
-      buildRsvpString(
-        activeRsvp.eventName,
-        activeRsvp.dateString,
-        activeRsvp.coming,
-        activeRsvp.notComing
-      )
+    buildDisabledRsvpString(
+      activeRsvp.eventName,
+      activeRsvp.dateString,
+      activeRsvp.coming,
+      activeRsvp.notComing
     ),
     Extra.markdown()
   );
   activeRsvp = null;
 };
 
+/**
+ * Update message for active RSVP.
+ * @param {*} ctx 
+ */
 const updateRsvpMessage = ctx => {
   ctx.editMessageText(
     buildRsvpString(
@@ -120,24 +150,30 @@ const updateRsvpMessage = ctx => {
   );
 };
 
+/**
+ * Infinite loop that runs with a fixed delay between iterations.
+ * In each iteration, check if it is time to send out a scheduled event or disable an active RSVP.
+ */
 const run = () => {
   console.log("Checking if should send message...");
   const now = new Date();
   const scheduledEvent = getEvent(now);
   if (scheduledEvent && !foundDateInArray(scheduledEvent.date, sentDates)) {
+    // Reset active RSVP
     if (activeRsvp) {
       disableOldRsvp();
     }
-    console.log("Sending message...");
-    const message = buildNewRsvpString(
-      scheduledEvent.eventName,
-      scheduledEvent.dateString
-    );
     activeRsvp = {
       ...scheduledEvent,
       coming: [],
       notComing: []
     };
+    // Send out new RSVP
+    console.log("Sending message...");
+    const message = buildNewRsvpString(
+      scheduledEvent.eventName,
+      scheduledEvent.dateString
+    );
     bot.telegram
       .sendMessage(process.env.CHAT_ID, message, defaultRsvpMenu)
       .then(m => {
@@ -147,6 +183,7 @@ const run = () => {
       }); // TODO: handle promise rejection
     sentDates.push(activeRsvp.date);
   }
+
   console.log("Checking if should disable old RSVPs...");
   if (activeRsvp && isSameDate(activeRsvp.deadline, now)) {
     disableOldRsvp();
